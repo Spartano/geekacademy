0eb30a45c67e90fb77e3440f1316e2bc75b1ceea
Gioele <Gioeledv@gmail.com>
2021-04-15 21:52:20 +0200

    primo commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// express app
+const app = express();
+// listen for requests
+app.listen(3000);
+app.get("/", (req, res) => {
+  // res.send('<p>home page</p>');
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+  // res.send('<p>about page</p>');
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+app.get("/error", (req, res) => {
+  // res.send('<p>about page</p>');
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+// redirects
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile("./views/error.html", { root: __dirname });
+});
