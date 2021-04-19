bf19e409f16117e9a017d3af17757e1589435463
Victor <nasutz8@gmail.com>
2021-04-19 12:50:37 +0200

    Middlewares

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
@@ -8,13 +9,35 @@ app.listen(3000);
 
 // register view engine
 app.set("view engine", "ejs");
-// app.set('views', 'myviews');
+
+// middleware & static files
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  console.log("new request made:");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in the next middleware");
+  next();
+});
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 app.get("/", (req, res) => {
   const blogs = [
-    { title: "Yoshi ", snippet: "Lorem ipsum dolor sit amet consectetur" },
-    { title: "Mario", snippet: "Lorem ipsum dolor sit amet consectetur" },
-    { title: "Bowser", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    { title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    { title: "Mario finds stars", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    { title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur" },
   ];
   res.render("index", { title: "Home", blogs });
 });
