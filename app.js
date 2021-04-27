e94f0534ba3af412a1abcb407788b285742d46ce
Andrea <andrew.fede25@gmail.com>
2021-04-27 22:42:14 +0200

    andrea MCV

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const blogRoutes = require("./routes/blogRoutes");
 
-app.listen(3000, () => {
-    console.log("Sto girando sulla porta 3000!");
-});
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://andrea:SdHqYENiQeCNpqBT@cluster0.o6lu0.mongodb.net/myFirstDatabase";
+//SdHqYENiQeCNpqBT
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    // listen for requests
+
+    app.listen(3000, () => {
+      console.log("connection established");
+    });
+  })
+  .catch((err) => console.log(err));
+
 
 // register view engine
 app.set("view engine", "ejs");
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,56 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { randomBytes } = require("crypto");
-const _ = require("lodash");
 
-// variabile presente nella memoria del processo, se riavvio il server torna vuoto
-const blogs = [];
+const 
+{
+  blog_index,
+  blog_details,
+  blog_create_get,
+  blog_create_post,
+  blog_delete,
+} = require("../controllers/BlogController");
 
-router.get("/blogs", (req, res) => {
-    res.render("index", 
-    {   
-        title: "Super Mario", 
-        blogs,
-    });
-});
+router.get("/blogs", blog_index);
 
-router.post("/blogs", (req, res) => {
-    // proprietà aggiunta dal middleware express.urlencoded che raccoglie i dati dal form 
-    const data = req.body;
+router.get("/blogs", blog_details);
 
-    
-    data.id = randomBytes(4).toString("hex");
-    blogs.push(data);
+router.get("/blogs/create", blog_create_get);
 
+router.get("/blogs/:id", blog_create_post);
 
-    console.log(data);
-
-    res.redirect("/blogs");
-});
-
-router.get("/blogs/create", (req, res) => {
-    res.render("create", { title: "Super Mario" });
-});
-
-// :id é un parametro della route (dinamicità)
-router.get("/blogs/:id", (req, res) => {
-    const id = req.params.id;
-
-    const blog = blogs.find((blog) => blog.id === id);
-
-    res.render("details", { blog, title: "Blog Details" });
-});
-
-router.delete("/blogs/:id", (req, res) => {
-    const id = req.params.id;
-   
-    _.remove(blogs, function(blog) {
-        return blog.id === id;
-    });
-
-    // rimando dei dati (object) al front-end che decide cosa fare
-    res.json({ redirect: "/blogs"});
-});
-
+router.get("/blogs/:id", blog_delete);
 
 module.exports = router;
